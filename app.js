55b3c2bf8e7993e45aa651612f1dc16bd555e251
Obiora Odimmegwa <48391689+ObioraOdimmegwa@users.noreply.github.com>
2022-08-24 16:16:21 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+var inverval_timer;
+
+//Time in milliseconds [1 second = 1000 milliseconds ]    
+inverval_timer = setInterval(function() { 
+    
+}, 2000);
+
+
+    
+
+function stop_timer() {
+    clearInterval(inverval_timer); 
+}
